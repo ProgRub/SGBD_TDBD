2dbf36e590594c4e3c1dfc3cb89951bd8b6b5ee4
ProgRub <ruben2000jgr@gmail.com>
2020-12-19 07:45:00 +0000

    Gestão de itens e unidades feito (?)

--- custom/php/gestao-de-itens.php
+++ custom/php/gestao-de-itens.php
@@ -1,5 +1,5 @@
 <?php
-echo "MUDOU\n";
+echo "MUDOU2\n";
 require_once("custom/php/common.php");
 
 if (verificaCapability("manage_items")) {
@@ -23,38 +23,37 @@ if (verificaCapability("manage_items")) {
                     echo "Inseriu os dados de novo item com sucesso.\nClique em Continuar para avançar.";
                     echo "<br><a href='gestao-de-itens'>Continuar</a>";
                 }
-            }else{
+            } else {
                 echo "O campo 'Nome' é obrigatório!\n";
                 voltarAtras();
             }
         } else {
-            $queryItens = "SELECT * FROM item"; //TODOS OS ITENS
-            $tabelaItens = mysqli_query($mySQL, $queryItens);
+//            $queryItens = "SELECT * FROM item"; //TODOS OS ITENS
+//            $tabelaItens = mysqli_query($mySQL, $queryItens);
 
-            if (mysqli_num_rows($tabelaItens) > 0) { //sucesso na query
+            if (mysqli_num_rows(mysqli_query($mySQL, "SELECT * FROM item")) > 0) { //ver se há items na tabela item
                 $queryTipos = "SELECT * FROM item_type ORDER BY name"; //TODOS OS TIPOS DE ITENS
                 $tabelaTipos = mysqli_query($mySQL, $queryTipos);
-
                 if (mysqli_num_rows($tabelaTipos) > 0) {
                     echo "<table>";
                     echo "<tr><th>tipo de item</th><th>id</th><th>nome do item</th><th>estado</th><th>ação</th></tr>";
 
-                    while ($linhaTipo = mysqli_fetch_assoc($tabelaTipos)) { //CADA TIPO DE ITEM
-                        $queryItensTipo = "SELECT * FROM item WHERE item_type_id = " . $linhaTipo["id"] . " ORDER BY name"; //TODOS OS ITENS DE DESSE TIPO
-                        $tabelaItensTipo = mysqli_query($mySQL, $queryItensTipo);
+                    while ($linhaTipoItem = mysqli_fetch_assoc($tabelaTipos)) { //CADA TIPO DE ITEM
+                        $queryItens = "SELECT * FROM item WHERE item_type_id = " . $linhaTipoItem["id"] . " ORDER BY name"; //TODOS OS ITENS DESSE TIPO
+                        $tabelaItens = mysqli_query($mySQL, $queryItens);
 
-                        if (mysqli_num_rows($tabelaItensTipo) > 0) {
-                            $newItem = true;
-                            $numeroItens = mysqli_num_rows($tabelaItensTipo);
-                            while ($linhaItemTipo = mysqli_fetch_assoc($tabelaItensTipo)) {
-                                echo "<tr><td>".$linhaTipo["name"]."</td><td>".$linhaItemTipo["id"]."</td><td>".$linhaItemTipo["name"]."</td><td>".($linhaItemTipo["state"]=='active'?'ativo':'inativo')."</td><td>[editar] [desativar]</td></tr>";
+                        if (mysqli_num_rows($tabelaItens) > 0) {
+//                            $newItem = true;
+//                            $numeroItens = mysqli_num_rows($tabelaItens);
+                            while ($linhaItem = mysqli_fetch_assoc($tabelaItens)) {
+                                echo "<tr><td>" . $linhaTipoItem["name"] . "</td><td>" . $linhaItem["id"] . "</td><td>" . $linhaItem["name"] . "</td><td>" . ($linhaItem["state"] == 'active' ? 'ativo' : 'inativo') . "</td><td>[editar] [desativar]</td></tr>";
 //                                if ($newItem) {
-//                                    echo "<tr><td rowspan='$numeroItens'>" . $linhaTipo["name"] . "</td>"; //NOME DESSE TIPO
+//                                    echo "<tr><td rowspan='$numeroItens'>" . $linhaTipoItem["name"] . "</td>"; //NOME DESSE TIPO
 //                                    $newItem = false;
 //                                } else {
 //                                    echo "<tr>";
 //                                }
-//                                echo "<td>" . $linhaItemTipo["id"] . "</td><td>" . $linhaItemTipo["name"] . "</td><td>" . ($linhaItemTipo["state"] == 'active' ? 'ativo' : 'inativo') . "</td><td>[editar] [desativar]</td>"; //DADOS DE CADA ITEM DESSE TIPO
+//                                echo "<td>" . $linhaItem["id"] . "</td><td>" . $linhaItem["name"] . "</td><td>" . ($linhaItem["state"] == 'active' ? 'ativo' : 'inativo') . "</td><td>[editar] [desativar]</td>"; //DADOS DE CADA ITEM DESSE TIPO
 //                                echo "</tr>";
                             }
                         }
